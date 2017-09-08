3ec11ac99fe5294a99adcffd35368b0cdec8c84b
dhope21 <dhope21@gmail.com>
2017-09-08 20:25:44 +0530

    showing paused distance with different color

--- src/components/map.js
+++ src/components/map.js
@@ -10,9 +10,9 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
     <GoogleMap
         ref={props.onMapLoad}
         defaultZoom={16}
-        defaultCenter={props.markers[0]}
+        defaultCenter={props.markers[0][0].finalData[0].position}
         onClick={props.onMapClick}
-        center={props.markers[0]}
+        center={props.markers[0][0].finalData[0].position}
     >
 
         {/* {props.markers.map((marker, index) => 
@@ -29,21 +29,52 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
 
             (
                 <Marker
-                    position={index === 0 || index === props.markers.length - 1 ? marker : null}
+                    position={index === 0 || index === marker[0].finalData.length - 1 ? marker[0].finalData[0].position : null}
                     key={index}
                     onRightClick={() => props.onMarkerRightClick(index)}
                 />
             )
-            )
+        )
+        }
+        
+
+        {
+            props.markers.map((marker, index) =>
+
+                 (
+                    
+                    <Polyline
+                        path={marker[0].finalData.map((finalData, index) => {
+
+                            return finalData.position
+                        })}
+                        geodesic={true}
+                        options={{strokeColor:"#ff0000",strokeWeight:"5",strokeOpacity:"0.6"}}
+                        key = {index}
+                    />
+                    
+                 )   
+        )
         }
 
-        <Polyline
-            path={props.markers.map((marker, index) => {
-                return marker
-            })}
-            geodesic={true}
-            options={{strokeColor:"#ff0000",strokeWeight:"5",strokeOpacity:"0.6"}}
-        />
+        {
+            props.markers.map((marker, index) =>
+
+                 (
+                    
+                    <Polyline
+                        path={marker[1].finalData.map((finalData, index) => {
+
+                            return finalData.position
+                        })}
+                        geodesic={true}
+                        options={{strokeColor:"#416ef4",strokeWeight:"5",strokeOpacity:"0.8"}}
+                        key = {index}
+                    />
+                 )   
+        )
+        }
+
 
 
     </GoogleMap>
@@ -57,22 +88,42 @@ export default class GoogleMap2 extends Component {
 
     render() {
         var finalData = [];
+        var finalDataObject = {};
+        var last_location = null;
+
         function getLocation(state) {
             
             function combineBatch(n) {
                 var location = {
+                    position: {
                         lat: n.lat,
                         lng: n.lon
+                    }
 
                 }
                 return location;
             }
-            finalData = _.concat(finalData,_.map(state.location_array, combineBatch));
-            console.log("Final Data",finalData.length);
-            return finalData;
+            // finalData = _.concat(finalData,_.map(state.location_array, combineBatch));
+            finalData = _.map(state.location_array, combineBatch);
+            var paused_data = [];
+            var paused_data_object = {};
+            if (last_location){
+                paused_data = [last_location, _.first(finalData)]
+                paused_data_object = {finalData:paused_data};
+                console.log('paused_data',paused_data,paused_data_object);
+            } else {
+                paused_data = [ _.first(finalData), _.first(finalData)]
+                paused_data_object = {finalData:paused_data};
+            }
+            last_location = _.last(finalData);
+            finalDataObject = {finalData:finalData};
+            console.log('paused_data',paused_data);
+            console.log("Final Data",finalData.length,finalDataObject);
+            finalDataObject = _.concat(finalDataObject,paused_data_object);
+            //  concat paused_data
+            return finalDataObject;
 
         }
-
 
         if (this.props.location != null && this.props.location != "" && this.props.location.count > 0) {
             var state = this.props.location.results;
@@ -94,7 +145,8 @@ export default class GoogleMap2 extends Component {
 
                         onMarkerRightClick={_.noop}
 
-                        markers={_.last(finalData)}
+                        // markers={_.last(finalData)}
+                        markers={finalData}
 
                     />
                 </div>
