d3fa3ae29086e6e3ec0c0618952c42acf12eca7e
dhope21 <dhope21@gmail.com>
2017-10-02 22:44:40 +0530

    added distance, steps and duration on button click

---
+++ src/dashboards/leaguecausedata.js
@@ -0,0 +1,410 @@
+module.exports = {
+	 leagues_static_data :[
+				{            
+		            "id": 10,
+		            "impactleague_name": "Run to Kill Hunger",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/10/hunger-league-banner.png",
+	            },
+	            {            
+		            "id": 2,
+		            "impactleague_name": "Impact Run Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/2/bizongo-IL-banner.jpg",
+	            },
+	            {            
+		            "id": 4,
+		            "impactleague_name": "Hero Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/None/HILbanner.jpg",
+	            },
+	            {            
+		            "id": 7,
+		            "impactleague_name": "Aarti Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/7/aarti_impactleague_banner2.jpg",
+	            },
+	            {            
+		            "id": 8,
+		            "impactleague_name": "SuperHero League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/8/superhero-league-banner_2.png",
+	            },
+	            {            
+		            "id": 3,
+		            "impactleague_name": "Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/3/Reliance_IL_banner_f0dGc8v.jpg",
+	            },
+	            {            
+		            "id": 6,
+		            "impactleague_name": "Ghoomakad Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/6/ghoomakad_il_banner.jpg",
+	            },
+	            {            
+		            "id": 1,
+		            "impactleague_name": "Genpact Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/1/notif2.jpg",
+	            },
+	            {            
+		            "id": 9,
+		            "impactleague_name": "Sample Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/None/HILbanner_rXBdQ8v.jpg",
+	            },
+	            {            
+		            "id": 13,
+		            "impactleague_name": "Startup Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/13/Start-Up-banner_f.jpg",
+	            },
+	            {            
+		            "id": 15,
+		            "impactleague_name": "Global Shapers Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/15/"
+	            },
+	            {            
+		            "id": 12,
+		            "impactleague_name": "Inter Hostel Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/12/bannerv3.jpg",
+	            },
+	            {            
+		            "id": 16,
+		            "impactleague_name": "LTI Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/None/LTI_Banner.JPG",
+	            },
+	            {            
+		            "id": 14,
+		            "impactleague_name": "Jubilant Impact League",
+		            "impactleague_banner": null,
+	            },
+	            {            
+		            "id": 5,
+		            "impactleague_name": "Kota Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/5/kota-league-banner.jpg",
+	            },
+	            {            
+		            "id": 11,
+		            "impactleague_name": "Inter IIT Impact League",
+		            "impactleague_banner": "http://dev.impactrun.com/media/photos/11/inter-IIT-banner_f2.jpg",
+	            }
+		]
+		// ,
+		// hostel :[            {
+  //           "id": 271,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 1",
+  //           },
+  //           {
+  //           "id": 272,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 2",
+  //           },
+  //           {
+  //           "id": 273,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 3",
+  //           },
+  //           {
+  //           "id": 274,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 4",
+  //           },
+  //           {
+  //           "id": 276,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 6",
+  //           },
+  //           {
+  //           "id": 275,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 5",
+  //           },
+  //           {
+  //           "id": 277,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 7",
+  //           },
+  //           {
+  //           "id": 278,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 8",
+  //           },
+  //           {
+  //           "id": 279,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 9",
+  //           },
+  //           {
+  //           "id": 280,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 10",
+  //           },
+  //           {
+  //           "id": 281,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 11",
+  //           },
+  //           {
+  //           "id": 282,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 12",
+  //           },
+  //           {
+  //           "id": 283,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 13",
+  //           },
+  //           {
+  //           "id": 284,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 14",
+  //           },
+  //           {
+  //           "id": 285,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 15",
+  //           },
+  //           {
+  //           "id": 286,
+  //           "impactleague": 12,
+  //           "team_name": "Tansa",
+  //           },
+  //           {
+  //           "id": 287,
+  //           "impactleague": 12,
+  //           "team_name": "Hostel 16",
+  //           }
+		// ],
+		// startup: [            {
+  //           "id": 289,
+  //           "impactleague": 13,
+  //           "team_name": "Qriyo",
+  //           },
+  //           {
+  //           "id": 290,
+  //           "impactleague": 13,
+  //           "team_name": "Toppr",
+  //           },
+  //           {
+  //           "id": 292,
+  //           "impactleague": 13,
+  //           "team_name": "Flyrobe",
+  //           },
+  //           {
+  //           "id": 295,
+  //           "impactleague": 13,
+  //           "team_name": "Aasaan Jobs",
+  //           },
+  //           {
+  //           "id": 293,
+  //           "impactleague": 13,
+  //           "team_name": "Coverfox",
+  //           },
+  //           {
+  //           "id": 301,
+  //           "impactleague": 13,
+  //           "team_name": "Saral Designs",
+  //           },
+  //           {
+  //           "id": 291,
+  //           "impactleague": 13,
+  //           "team_name": "Bizongo",
+  //           },
+  //           {
+  //           "id": 296,
+  //           "impactleague": 13,
+  //           "team_name": "Minimalist",
+  //           },
+  //           {
+  //           "id": 294,
+  //           "impactleague": 13,
+  //           "team_name": "Bewakoof",
+  //           },
+  //           {
+  //           "id": 331,
+  //           "impactleague": 13,
+  //           "team_name": "Genius",
+  //           },
+  //           {
+  //           "id": 332,
+  //           "impactleague": 13,
+  //           "team_name": "Impact",
+  //           },
+  //           {
+  //           "id": 333,
+  //           "impactleague": 13,
+  //           "team_name": "Ithaka",
+  //           },
+  //           {
+  //           "id": 344,
+  //           "impactleague": 13,
+  //           "team_name": "Dozee",
+  //           },
+  //           {
+  //           "id": 346,
+  //           "impactleague": 13,
+  //           "team_name": "Indiez",
+  //           },
+  //           {
+  //           "id": 345,
+  //           "impactleague": 13,
+  //           "team_name": "SandBox",
+  //           },
+  //           {
+  //           "id": 343,
+  //           "impactleague": 13,
+  //           "team_name": "Yumlane",
+  //           }
+		// ],
+
+		// kota: [
+		// {
+  //               "id": 122,
+  //               "impactleague": 5,
+  //               "team_name": "DCM",
+  //           },
+  //           {
+  //               "id": 123,
+  //               "impactleague": 5,
+  //               "team_name": "KOTA",
+  //           },
+  //           {
+  //               "id": 127,
+  //               "impactleague": 5,
+  //               "team_name": "JAIPUR",
+  //           }
+
+		// ], 
+
+		// inter_iit: [            {
+  //               "id": 313,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Bombay",
+  //           },
+  //           {
+
+  //               "id": 309,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Kanpur",
+  //           },
+  //           {
+
+  //               "id": 311,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Kharagpur",
+  //           },
+  //           {
+
+  //               "id": 314,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Patna",
+  //           },
+  //           {
+
+  //               "id": 310,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Jodhpur",
+  //           },
+  //           {
+
+  //               "id": 308,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Indore",
+  //           },
+  //           {
+
+  //               "id": 307,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Guwahati",
+  //           },
+  //           {
+
+  //               "id": 305,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Madras",
+  //           },
+  //           {
+
+  //               "id": 303,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Bhubaneswar",
+  //           },
+  //           {
+
+  //               "id": 302,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Gandhinagar",
+  //           },
+  //           {
+
+  //               "id": 312,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Hyderabad",
+  //           },
+  //           {
+
+  //               "id": 339,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Dharwad",
+  //           },
+  //           {
+
+  //               "id": 315,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Delhi",
+  //           },
+  //           {
+
+  //               "id": 338,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Bhilai",
+  //           },
+  //           {
+
+  //               "id": 337,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Goa",
+  //           },
+  //           {
+
+  //               "id": 336,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Tirupati",
+  //           },
+  //           {
+
+  //               "id": 335,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Palakkad",
+  //           },
+  //           {
+
+  //               "id": 320,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Dhanbad",
+  //           },
+  //           {
+
+  //               "id": 319,
+  //               "impactleague": 11,
+  //               "team_name": "IIT BHU",
+  //           },
+  //           {
+
+  //               "id": 318,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Roorkee",
+  //           },
+  //           {
+
+  //               "id": 316,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Ropar",
+  //           },
+  //           {
+
+  //               "id": 317,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Mandi",
+  //           },
+  //           {
+
+  //               "id": 334,
+  //               "impactleague": 11,
+  //               "team_name": "IIT Jammu",
+  //           }]
+
+	}
