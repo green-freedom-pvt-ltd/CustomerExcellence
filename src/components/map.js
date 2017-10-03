7bf25e3a5f1fdb3b264e7a29e52c6e958db5f8e4
nishant khandelwal <nishantkhandelwal69@gmail.com>
2017-10-04 00:42:40 +0530

    Added start and End Label

--- src/components/map.js
+++ src/components/map.js
@@ -33,6 +33,7 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
                     position={index === 0 ? marker[0].finalData[0].position : null}
                     key={index}
                     onRightClick={() => props.onMarkerRightClick(index)}
+                    label="E"
                 />
             )
         )
@@ -45,47 +46,48 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
                     position={index === props.markers.length - 1 ? marker[0].finalData[(marker[0].finalData.length - 1)].position : null}
                     key={index}
                     onRightClick={() => props.onMarkerRightClick(index)}
+                    label="S"
                 />
             )
         )
         }
-        
+
 
         {
             props.markers.map((marker, index) =>
 
-                 (
-                    
+                (
+
                     <Polyline
                         path={marker[0].finalData.map((finalData, index) => {
 
                             return finalData.position
                         })}
                         geodesic={true}
-                        options={{strokeColor:"#ff0000",strokeWeight:"5",strokeOpacity:"0.6"}}
-                        key = {index}
+                        options={{ strokeColor: "#ff0000", strokeWeight: "5", strokeOpacity: "0.6" }}
+                        key={index}
                     />
-                    
-                 )   
-        )
+
+                )
+            )
         }
 
         {
             props.markers.map((marker, index) =>
 
-                 (
-                    
+                (
+
                     <Polyline
                         path={marker[1].finalData.map((finalData, index) => {
 
                             return finalData.position
                         })}
                         geodesic={true}
-                        options={{strokeColor:"#416ef4",strokeWeight:"5",strokeOpacity:"0.8"}}
-                        key = {index}
+                        options={{ strokeColor: "#416ef4", strokeWeight: "5", strokeOpacity: "0.8" }}
+                        key={index}
                     />
-                 )   
-        )
+                )
+            )
         }
 
 
@@ -105,7 +107,7 @@ export default class GoogleMap2 extends Component {
         var last_location = null;
 
         function getLocation(state) {
-            
+
             function combineBatch(n) {
                 var location = {
                     position: {
@@ -120,23 +122,23 @@ export default class GoogleMap2 extends Component {
             finalData = _.map(state.location_array, combineBatch);
             var paused_data = [];
             var paused_data_object = {};
-            finalDataObject = {finalData:_.reverse(finalData)};
+            finalDataObject = { finalData: _.reverse(finalData) };
             // console.log('paused_data',paused_data);
             // console.log("Final Data",finalData.length,finalDataObject);
-            if (last_location){
-                if(_.first(finalData)){
+            if (last_location) {
+                if (_.first(finalData)) {
                     paused_data = [last_location, _.first(finalData)]
                 } else {
-                    paused_data = [last_location,last_location]
+                    paused_data = [last_location, last_location]
                 }
-                paused_data_object = {finalData:paused_data};
-                console.log('paused_data',paused_data,paused_data_object);
+                paused_data_object = { finalData: paused_data };
+                console.log('paused_data', paused_data, paused_data_object);
             } else {
-                paused_data = [ _.first(finalData), _.first(finalData)]
-                paused_data_object = {finalData:paused_data};
+                paused_data = [_.first(finalData), _.first(finalData)]
+                paused_data_object = { finalData: paused_data };
             }
             last_location = _.last(finalData);
-            finalDataObject = _.concat(finalDataObject,paused_data_object);
+            finalDataObject = _.concat(finalDataObject, paused_data_object);
             //  concat paused_data
             return finalDataObject;
 
@@ -146,11 +148,11 @@ export default class GoogleMap2 extends Component {
         console.log("DATA", this.props);
         finalData = _.map(state, getLocation);
 
-        finalData = _.remove(finalData, function(n) {
-              return n[0].finalData.length > 0;
-            });
-        console.log("Final Data",finalData);
-        if (this.props.location != null && this.props.location != "" && this.props.location.count > 0 && finalData.length >0) {
+        finalData = _.remove(finalData, function (n) {
+            return n[0].finalData.length > 0;
+        });
+        console.log("Final Data", finalData);
+        if (this.props.location != null && this.props.location != "" && this.props.location.count > 0 && finalData.length > 0) {
             return (
                 <div style={{ height: `100%` }}>
 
