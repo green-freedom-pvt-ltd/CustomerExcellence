8a8f6ba3e7b8c7f77ea63081c5cadc9b1bb400ea
dhope21 <dhope21@gmail.com>
2017-10-03 17:19:07 +0530

    1. added date range filter 2. added different search parameters

--- src/dashboards/dashboard.js
+++ src/dashboards/dashboard.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import { Button, Grid, Row, Col, Table, FormControl,Form, FormGroup,ControlLabel } from 'react-bootstrap';
-import {BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import './info.css';
 import _ from "lodash";
-import Overall from './overall'
+import Overall from './overall';
 
 export default class Dashboard extends Component{
 	
